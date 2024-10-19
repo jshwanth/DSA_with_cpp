0fc4566c786144523332696f5a1172b8157a463d
jshwanth <sjashwanth488@gmail.com>
2024-10-19 23:23:33 +0530

    Leetcode 238

---
+++ Arrays/product_of_array_except_self.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        vector<int> prefix(n, 1);
+        vector<int> suffix(n, 1);
+
+        // Calculate prefix products
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        // Calculate suffix products
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+
+        // Calculate the final answer
+        for (int i = 0; i < n; i++) {
+            ans[i] = prefix[i] * suffix[i];
+        }
+        
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> result = solution.productExceptSelf(nums);
+
+    cout << "Product of array except self: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
