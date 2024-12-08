c1a5837727d5176feb8c5ffba3d61c15d61a48cb
jshwanth <sjashwanth488@gmail.com>
2024-12-08 21:15:26 +0530

    leetcode 8

--- Strings/atoi.cpp
+++ Strings/atoi.cpp
@@ -8,21 +8,17 @@ int myAtoi(string s) {
     long result = 0;
     int sign = 1;
 
-    // Skip leading whitespaces
     while (i < n && s[i] == ' ') i++;
 
-    // Handle sign
     if (i < n && (s[i] == '+' || s[i] == '-')) {
         sign = (s[i] == '-') ? -1 : 1;
         i++;
     }
 
-    // Convert digits to number
     while (i < n && isdigit(s[i])) {
         result = result * 10 + (s[i] - '0');
         i++;
 
-        // Handle overflow
         if (result * sign > INT_MAX) return INT_MAX;
         if (result * sign < INT_MIN) return INT_MIN;
     }
@@ -31,7 +27,7 @@ int myAtoi(string s) {
 }
 
 int main() {
-    string s = "   -42";
+    string s = "   -42   ";
     cout << "Converted integer: " << myAtoi(s) << endl;
     return 0;
 }
